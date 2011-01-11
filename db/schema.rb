2cd7fcef8f41a1f9dd5e04f79f39044b9ffe4653
TinNguyen <tin.trung.nguyen@techpropulsionlabs.com>
2011-01-11 16:02:25 +0700

    add test data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110101151530) do
+ActiveRecord::Schema.define(:version => 20110111084736) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
