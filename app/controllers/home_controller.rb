faf1a01f0d54144e8ae01f3ece58e27220980912
Tin Nguyen <tin.trung.nguyen@techpropulsionlabs.com>
2011-01-15 23:17:03 +0700

    update view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
 		@posts = Post.paginate :page => params[:page], :order => "created_at DESC"
+		p = Post.find(4)
+		puts p.posted_for
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,46 @@
 class Post < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 4
+	attr_accessor :posted_for, :current_time, :posted_time
+
+	def posted_for			
+		@current_time = Time.now
+		@posted_time = self.created_at
+		if (@current_time.year - @posted_time.year) > 0
+			year_period
+		else	if (@current_time.year - @posted_time.year == 0) and (@current_time.month - @posted_time.month > 0)
+						month_period
+					else day_period		       		
+					end
+		end
+	end
+
+	protected
+	def year_period
+		case @current_time.year - @posted_time.year
+		when 1
+			@posted_for = (@current_time.year - @posted_time.year).to_s + " year ago"
+		else 2..10
+			@posted_for = (@current_time.year - @posted_time.year).to_s + " years ago"
+		end
+	end
+
+	def month_period
+		case @current_time.month - @posted_time.month
+		when 1
+			@posted_for = (@current_time.month - @posted_time.month).to_s + " month ago"
+		else 2..11
+			@posted_for = (@current_time.month - @posted_time.month).to_s + " months ago"
+		end
+	end
+	
+	def day_period
+		case @current_time.day - @posted_time.day
+		when 1
+			@posted_for = (@current_time.day - @posted_time.day).to_s + " day ago"
+		else 2..30
+			@posted_for = (@current_time.day - @posted_time.day).to_s + " days ago"
+		end
+	end
+
 end
